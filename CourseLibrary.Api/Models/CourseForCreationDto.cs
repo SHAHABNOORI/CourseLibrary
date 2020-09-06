32d1923d04fdc44878339ad992e74f270ea58033
NooriGoodarzi <NooriGoodarzi@gmail.com>
2020-09-06 17:13:52 +0430

    Some Validation Rules

--- CourseLibrary.Api/Models/CourseForCreationDto.cs
+++ CourseLibrary.Api/Models/CourseForCreationDto.cs
@@ -1,9 +1,27 @@
-﻿namespace CourseLibrary.API.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
+
+namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto /*: IValidatableObject*/
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
